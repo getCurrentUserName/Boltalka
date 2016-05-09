e0fa53e2addd01a4eb0087a92dcca80623cdff5e
GetCurrentUserEmail <GetCurrentUserEmail@gmail.com>
2016-05-09 15:31:02 +0900

    Home Page

--- src/main/java/com/boltalka/web/views/Index.java
+++ src/main/java/com/boltalka/web/views/Index.java
@@ -1,7 +1,13 @@
 package com.boltalka.web.views;
 
+import com.boltalka.domain.entities.user.User;
+import com.boltalka.services.user.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.security.Principal;
 
 /**
  * Created by LucidMinds on 07.05.16.
@@ -10,9 +16,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class Index {
 
+    @Autowired
+    UserDetailsServiceImpl userDetailsService;
     @RequestMapping("/")
-    public String index() {
-        return "index";
+    public ModelAndView index(Principal principal, ModelAndView modelAndView) {
+        if (principal == null) {
+            return new ModelAndView("index");
+        }
+        modelAndView.setViewName("index");
+        User user = userDetailsService.getCurrentUser();
+        modelAndView.addObject("username", user.getUsername());
+        modelAndView.addObject("nickname", user.getNickname());
+        return modelAndView;
     }
 
     @RequestMapping("room")
---
+++ src/main/webapp/views/index.jsp
@@ -0,0 +1,3 @@
+<h1>Hello</h1>
+<h1>username ${username}</h1>
+<h1>nickname ${nickname}</h1>
